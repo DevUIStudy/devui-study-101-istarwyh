612e10ba2aea731af3a022a632d16e0d22d78216
istarwyh <yihui.wang@pku.edu.cn>
2019-11-12 16:23:37 +0800

    Add: add rough data table

--- src/app/admin-files-chart/admin-files-chart.component.ts
+++ src/app/admin-files-chart/admin-files-chart.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import {SourceType,originSource } from './mock-data';
 @Component({
   selector: 'app-admin-files-chart',
   templateUrl: './admin-files-chart.component.html',
@@ -16,4 +16,44 @@ export class AdminFilesChartComponent implements OnInit {
     console.log(term);
   }
 
+  basicDataSource: Array<SourceType> = JSON.parse(JSON.stringify(originSource.slice(0, 6)));
+  dataTableOptions = {
+    columns: [
+        {
+            field: 'firstName',
+            header: '用户名称',
+            fieldType: 'text'
+        },
+        {
+            field: 'lastName',
+            header: '昵称',
+            fieldType: 'text'
+        },
+        {
+            field: 'gender',
+            header: '用户标签',
+            fieldType: 'text'
+        },
+        {
+            field: 'dob',
+            header: '企业用户',
+            fieldType: 'date'
+        },
+        {
+          field: 'dob',
+          header: '项目角色',
+          fieldType: 'date'
+        },
+        {
+          field: 'dob',
+          header: '账号体系',
+          fieldType: 'date'
+        },
+        {
+          field: 'dob',
+          header: '是否禁用',
+          fieldType: 'date'
+      }
+    ]
+  };
 }
