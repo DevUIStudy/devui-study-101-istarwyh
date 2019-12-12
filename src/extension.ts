e24ce7dff7ea62fb294428944c0ea81c9c59f028
istarwyh <yihui.wang@pku.edu.cn>
2019-12-12 21:18:13 +0800

    Warning: Donot redeclare vscode, but why? js->ts

--- src/extension.ts
+++ src/extension.ts
@@ -1,26 +1,20 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
+// import vscode from 'vscode';
 const vscode = require('vscode');
 
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
-
 /**
- * @param {vscode.ExtensionContext} context
+ * @param {*} context
  */
-function activate(context) {
+exports.activate =function(context){
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
 	console.log('Congratulations, your extension "AutoDevui" is now active!');
+	console.log(vscode);
+    require('./hover.ts')(context); // 悬停提示
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with  registerCommand
+	// console.error('(⊙﹏⊙)挂了');
+
+	
 	// The commandId parameter must match the command field in package.json
 	let disposable = vscode.commands.registerCommand('extension.helloWorld', function () {
-		// The code you place here will be executed every time your command is executed
-
-		// Display a message box to the user
 		vscode.window.showInformationMessage('Hello VS Code!');
 	});
 	context.subscriptions.push(disposable);
@@ -35,15 +29,7 @@ function activate(context) {
 		vscode.window.showInformationMessage(`当前文件(夹)路径是：${uri ? uri.path : '空'}`);
 	}));
 	
-}
-exports.activate = activate;
-
-// this method is called when your extension is deactivated
-function deactivate() {
-	console.log('它已经被释放了');
-}
-
-module.exports = {
-	activate,
-	deactivate
-}
+};
+exports.deactivate = function() {
+    console.log('您的扩展“AutoDevui”已被释放！')
+};
---
+++ src/params/button.ts
@@ -0,0 +1,3 @@
+export default{
+    bsStyle:'IButtonStyle|primary|【可选】风格 primary \| common \| text \| text-dark|'
+}
