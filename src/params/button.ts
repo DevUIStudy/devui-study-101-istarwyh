ebd726da8c38e4a655e8d4bac6a54e915642d00c
istarwyh <yihui.wang@pku.edu.cn>
2020-02-11 20:14:01 +0800

    Add: params

--- src/params/button.ts
+++ src/params/button.ts
@@ -1,6 +1,12 @@
 export default{
-    title:    '| 卡片标题 | string\|ReactNode   |  -  |',
-    extra:    '| 卡片右上角的操作区域 | string\|ReactNode   | - |',
-    bordered: '| 是否有边框 | boolean   |  true  |',
-    bodyStyle: '| 内容区域自定义样式 | object   |  -  |'
+id:     '|`string`|--|【可选】buttonid|',
+type:     '|`IButtonType`|button|【可选】类型\'button\'\|\'submit\'\|\'reset\'|',
+bsStyle:     '|`IButtonStyle`|primary|【可选】风格\'primary\'\|\'common\'\|\'text\'\|\'text-dark\'|',
+bsSize:     '|`IButtonSize`|\'sm\'|【可选】大小\'lg\'\|\'sm\'\|\'xs\'|',
+bordered:     '|`boolean`|false|【可选】是否有边框|',
+icon:     '|`string`|--|【可选】自定义按钮图标|',
+showLoading:     '|`boolean`|false|【可选】是否显示加载提示|',
+width:     '|`number`|--|【可选】button宽度|',
+disabled:     '|`boolean`|false|【可选】是否禁用button|',
+autofocus:     '|`boolean`|false|【可选】按钮加载时是否自动获得焦点|',
 };
--- src/params/index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-exports.__esModule = true;
-var card_1 = require("./card");
-var button_1 = require("./button");
-exports["default"]={
-    Card:card_1["default"],
-    Button:button_1["default"]
-};
